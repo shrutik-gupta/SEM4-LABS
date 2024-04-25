aa9e2701026a36be5860eb69e66f0157d0c77539
Shrutik Gupta <shrutikgupta07@gmail.com>
2024-04-25 23:29:38 +0530

    added more patterns

--- Python/Basics/basics-pattern.py
+++ Python/Basics/basics-pattern.py
@@ -4,3 +4,17 @@ for i in range(row+1):
     for j in range(i):
         print("*", end='')
     print("\n")
+print("--------------------------")
+
+for i in range(row+1):
+    for k in range(row-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*", end='')
+    print("\n")
+print("--------------------------")
+
+for i in range(row,0,-1):
+    for j in range(i):
+        print("*", end='')
+    print("\n")
